4ddf19802fa33a6506db86640552ea79b9516d89
Louis Cha <lcha@sfu.ca>
2020-03-14 02:39:20 -0700

    Handle upload file & customer info request, and add them to the database

--- client/src/component_lists/AddCustomer.js
+++ client/src/component_lists/AddCustomer.js
@@ -1,76 +1,88 @@
-import React from 'react';
+import React from 'react'
 import { post } from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+
 
 class AddCustomer extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            file: null,
-            fileName: "",
-            userName: "",
-            gender: "",
-            city: "",
-            job: "",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+        file: null,
+        fileName: "",
+        userName: "",
+        gender: "",
+        city: "",
+        job: ""
     }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.handleFileChange = this.handleFileChange.bind(this)
+    this.handleValueChange = this.handleValueChange.bind(this)
+    this.addCustomer = this.addCustomer.bind(this)
+  }
+  
+  handleFormSubmit(e) {
+    e.preventDefault()
+    this.addCustomer()
+    .then((response) => {
+      console.log(response.data);
+    })
+    this.setState({
+        file: null,
+        fileName: "",
+        userName: "",
+        gender: "",
+        city: "",
+        job: ""
+    })
+  }
 
-    handleForSubmit = (e) => {
-        e.preventDefault();
-        this.addCustomer().then((res) => {
-            console.log(res.data);
-        })
+  handleFileChange(e) {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value
+    });
+  }
+  
+  handleValueChange(e) {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
+  addCustomer(){
+    const url = '/api/customers';
+    const formData = new FormData();
+    formData.append('image', this.state.file);
+    formData.append('name', this.state.userName);
+    formData.append('gender', this.state.gender);
+    formData.append('city', this.state.city);
+    formData.append('job', this.state.job);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
     }
+    return post(url, formData, config)
+  }
 
-    handleValueChange = (e) => {
-        let nextState = {};
-        nextState[e.target.name] = e.target.value;
-        this.setState(nextState);
 
-    }
 
-    handleFileChange = (e) => {
-        this.setState({
-            // only select the first file to be uploaded.
-            file: e.target.files[0],
-            fileName: e.target.value
-        });
-    }
 
-    addCustomer = () => {
-        const url = '/api/customers';
-        const formData = new FormData();
-
-        formData.append('image', this.state.file);
-        formData.append('name', this.state.name);
-        formData.append('gender', this.state.gender);
-        formData.append('city', this.state.city);
-        formData.append('job', this.state.job);
-
-        const config = {
-            headers: {
-                // content type is multipart/form-data since the file is getting sent.
-                'content-type': 'multipart/form-data'
-            }
-        }
-
-        return post(url, formData, config);
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>Add Customer</h1>
-                Profile Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                City: <input type="text" name="city" value={this.state.city} onChange={this.handleValueChange}/><br/>
-                Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">Add</button>
-            </form>
-        )
-    }
+ 
+  render() {
+    return (
+        <form onSubmit={this.handleFormSubmit}>
+            <h1>Add Customer</h1>
+            Profile Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+            Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+            Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+            City: <input type="text" name="city" value={this.state.city} onChange={this.handleValueChange}/><br/>
+            Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+            <button type="submit">Add</button>
+        </form>
+    )
+    }   
 }
 
 export default AddCustomer;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const multer = require('multer');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -18,6 +20,28 @@ const dbConnection = sql.createConnection({
   port: conf.port,
   database: conf.database
 }); 
+
+const upload = multer({
+  dest: './upload'
+})
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = "INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)";
+  let image = '/image' + req.file.filename;
+  let name = req.body.userName;
+  let gender = req.body.gender;
+  let city = req.body.city;
+  let job = req.body.job;
+  let params = [image, name, gender, city, job];
+
+  dbConnection.query(sql, params, (err, row, field) => {
+    res.send(row);
+  });
+
+
+});
 
 dbConnection.connect();
 app.get('/api/customers', (req, res) => {
