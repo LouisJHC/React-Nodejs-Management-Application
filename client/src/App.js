39f00e644703893746cab9a07979acbf133d5799
Louis Cha <lcha@sfu.ca>
2020-03-17 18:07:03 -0700

    Added AppBar and changed the font

--- client/src/App.js
+++ client/src/App.js
@@ -11,19 +11,88 @@ import Paper from '@material-ui/core/Paper'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import AddCustomer from './component_lists/AddCustomer'
 
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import { fade } from '@material-ui/core/styles/colorManipulator'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    minwidth: 1080
   },
-
-  table: {
-    minWidth: 1080
-  },
-
   progress: {
     margin: theme.spacing.unit * 2
+  },
+  menu: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
+  },
+  grow: {
+    flexGrow: 1
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      },
+    },
   }
 });
 
@@ -72,19 +141,42 @@ class App extends React.Component {
   render() {
     // const classes = this.props.classes
     const { classes } = this.props;
+    const list = ["Id", "Image", "Name", "Gender", "City", "Job", "Setting"];
     return ( 
       <div>
+          <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+            <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              Customer Management System
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+        <AddCustomer refresh={this.refresh}/>
+      </div>
       <Paper className={classes.root}>
         <Table className={classes.table}>
            <TableHead>
              <TableRow>
-               <TableCell>Id</TableCell>
-               <TableCell>Image</TableCell>
-               <TableCell>Name</TableCell>
-               <TableCell>Gender</TableCell>
-               <TableCell>City</TableCell>
-               <TableCell>Job</TableCell>
-               <TableCell>Setting</TableCell>
+               {list.map(m => {
+                 return <TableCell className={classes.tableHead}>{m}</TableCell>
+               })}
              </TableRow>
            </TableHead>
            <TableBody>
@@ -118,7 +210,6 @@ class App extends React.Component {
             </TableBody>
           </Table> 
       </Paper>
-      <AddCustomer refresh = {this.refresh}/>
       </div>
     );
   }
